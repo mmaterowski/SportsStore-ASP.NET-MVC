9cdc235feb0845332b8ef61e384578418fe76c1d
mmaterowski <michal.materowski@gmail.com>
2018-10-01 17:22:50 +0200

    Adding category selection indicator (ui)

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -130,5 +130,26 @@ namespace SportsStore.UnitTests
 			Assert.AreEqual(results[1], "Oranges");
 			Assert.AreEqual(results[2], "Plums");
 		}
+
+		[TestMethod]
+		public void Indicates_Selected_Category()
+		{
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new Product[]
+			{
+				new Product {ProductID=1, Name="P1",Category="Apples" },
+				new Product {ProductID=4,Name="P2",Category="Oranges"},
+			});
+
+			NavController target = new NavController(mock.Object);
+
+			string categoryToSelect = "Apples";
+
+			string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+			Assert.AreEqual(categoryToSelect, result);
+		}
+
+
 	}
 }
