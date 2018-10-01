1f568152c80bff3f0b2f8d6eae86ad784b0ff979
mmaterowski <michal.materowski@gmail.com>
2018-10-01 12:34:55 +0200

    SettingUp Git

---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fcc17ac6-6641-4129-9f0b-b30692b0b6af")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+	public class ProductController : Controller
+	{
+		private IProductRepository repository;
+		public int PageSize = 4;
+
+		public ProductController(IProductRepository productRepository)
+		{
+			this.repository = productRepository;
+		}
+
+		public ViewResult List(int page = 1)
+		{
+			ProductListViewModel model = new ProductListViewModel
+			{
+				Products = repository.Products
+				.OrderBy(p => p.ProductID)
+				.Skip((page - 1) * PageSize)
+				.Take(PageSize),
+				PagingInfo = new PagingInfo
+				{
+					CurrentPage = page,
+					ItemsPerPage = PageSize,
+					TotalItems = repository.Products.Count()
+				}
+			};
+			return View(model);
+
+		}
+	}
+}
