25263dab1619c170b2a4f032a8299cc9424f59ba
mmaterowski <michal.materowski@gmail.com>
2018-10-01 16:45:53 +0200

    Adding category  PartialVeiw

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -6,6 +6,7 @@ using SportsStore.WebUI.Controllers;
 using SportsStore.WebUI.HtmlHelpers;
 using SportsStore.WebUI.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -105,6 +106,29 @@ namespace SportsStore.UnitTests
 			Assert.AreEqual(result.Length, 2);
 			Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
 			Assert.IsTrue(result[1].Name == "P4" && result[0].Category == "Cat2");
+		}
+
+		[TestMethod]
+		public void Can_Create_Categories()
+		{
+
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new Product[]
+			{
+				new Product {ProductID=1,Name="P1",Category="Apples" },
+				new Product {ProductID=2,Name="P2",Category="Apples" },
+				new Product {ProductID=3,Name="P3",Category="Plums" },
+				new Product {ProductID=4,Name="P4",Category="Oranges" },
+			});
+
+			NavController target = new NavController(mock.Object);
+
+			string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+			Assert.AreEqual(results.Length, 3);
+			Assert.AreEqual(results[0], "Apples");
+			Assert.AreEqual(results[1], "Oranges");
+			Assert.AreEqual(results[2], "Plums");
 		}
 	}
 }
--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -8,17 +8,16 @@ namespace SportsStore.WebUI
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
 			routes.MapRoute(null,
-				"",
-				new
-				{
-					controller = "Product",
-					action = "List",
-					category = (string)null,
-					page = 1
-				}
-			);
+			"",
+			new
+			{
+				controller = "Product",
+				action = "List",
+				category = (string)null,
+				page = 1
+			}
+		);
 
 			routes.MapRoute(null,
 				"Page{page}",
@@ -36,7 +35,9 @@ namespace SportsStore.WebUI
 				new { controller = "Product", action = "List" },
 				new { page = @"\d+" }
 			);
-			
+
+			routes.MapRoute(null, "{controller}/{action}");
+
 		}
 	}
 }
