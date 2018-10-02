3465524cd2bde358bcb4cf24d69459661519a055
mmaterowski <michal.materowski@gmail.com>
2018-10-02 08:22:00 +0200

    added custom binding for cart obj

--- SportsStore.UnitTests/CartTests.cs
+++ SportsStore.UnitTests/CartTests.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
 using System.Linq;
 using static SportsStore.Domain.Entities.Cart;
 
@@ -96,6 +99,25 @@ namespace SportsStore.UnitTests
 			target.Clear();
 
 			Assert.AreEqual(target.Lines.Count(), 0);
+		}
+
+		[TestMethod]
+		public void Can_Add_To_Cart()
+		{
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new Product[]
+			{
+				new Product {ProductID=1,Name="P1",Category="Apples" },
+			}.AsQueryable());
+
+			Cart cart = new Cart();
+
+			CartController target = new CartController(mock.Object);
+
+			target.AddToCart(cart, 1, null);
+
+			Assert.AreEqual(cart.Lines.Count(), 1);
+			Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
 		}
 	}
 }
--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Infrastructure.Binders;
 using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace SportsStore.WebUI
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
-    }
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(RouteTable.Routes); ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
+			ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
+		}
+	}
 }
