5be8f044265149772357fd3c070e8708d21c0a70
mmaterowski <michal.materowski@gmail.com>
2018-10-02 13:17:53 +0200

    Adding ShippingDetails model and front

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -48,5 +48,15 @@ namespace SportsStore.WebUI.Controllers
 			}
 			return RedirectToAction("Index", new { returnUrl });
 		}
+
+		public PartialViewResult Summary(Cart cart)
+		{
+			return PartialView(cart);
+		}
+
+		public ViewResult Checkout()
+		{
+			return View(new ShippingDetails());
+		}
 	}
 }
