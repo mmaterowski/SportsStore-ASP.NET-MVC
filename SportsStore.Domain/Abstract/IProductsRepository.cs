d6438788516417cd2218296af6dfc10cd02d0b98
mmaterowski <michal.materowski@gmail.com>
2018-10-04 09:35:09 +0200

    Adding Create and Delete methods for Admin panel

--- SportsStore.Domain/Abstract/IProductsRepository.cs
+++ SportsStore.Domain/Abstract/IProductsRepository.cs
@@ -8,5 +8,6 @@ namespace SportsStore.Domain.Abstract
 		IEnumerable<Product> Products { get; }
 
 		void SaveProduct(Product product);
+		Product DeleteProduct(int productID);
 	}
 }
--- SportsStore.UnitTests/AdminTests.cs
+++ SportsStore.UnitTests/AdminTests.cs
@@ -100,5 +100,25 @@ namespace SportsStore.UnitTests
 			mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never());
 			Assert.IsInstanceOfType(result, typeof(ViewResult));
 		}
+
+		[TestMethod]
+		public void Can_Delete_Valid_Products()
+		{
+			Product prod = new Product { ProductID = 2, Name = "Test" };
+
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new Product[]
+			{
+				new Product {ProductID=1,Name="P1"},
+				prod,
+				new Product {ProductID=3,Name="P3"},
+			});
+
+			AdminController target = new AdminController(mock.Object);
+
+			target.Delete(prod.ProductID);
+
+			mock.Verify(m => m.DeleteProduct(prod.ProductID));
+		}
 	}
 }
