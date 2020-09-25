8aa54a6e1809e0ae674c3037c280d5d3873b21c4
matisebixtroniksik <71881361+matisebixtroniksik@users.noreply.github.com>
2020-09-25 13:39:40 +0200

    Create LocationErrorHandler.cs

---
+++ SportsStore.Domain/Concrete/LocationErrorHandler.cs
@@ -0,0 +1,13 @@
+function handleLocationError(
+  browserHasGeolocation: boolean,
+  infoWindow: google.maps.InfoWindow,
+  pos: google.maps.LatLng
+) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
